62baae75249870efb90e1b47b08cc804d0c2c78f
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-07-10 23:41:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
   return "I can't hear you!"
-}
-
-function sayHiToGrandma(string) {
   if (string.toUpperCase())
   return "YES INDEED!"
 }
