78a0152687a5a823bb0ac64e0930c7f2f4e0cd9c
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-07-11 01:38:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
-  return "I can't hear you!"
-} else if (string.toUpperCase())
-  return "YES INDEED!"
-}function shout(string) {
+function shout(string) {
   return string.toUpperCase()
 }
 
@@ -19,3 +14,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() == string) {
+    return "I can't hear you!"
+  } else if (string.toUpperCase() == string) {
+    return "YES INDEED!"
+  } else if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  return "This is the default value"
+}
